dfe937b5240d9a60550d9e2a121097e9fcbe56d9
turtletear <ariqmr123@gmail.com>
2021-04-27 11:36:15 +0700

    create bluetooth fragment

--- app/src/main/java/com/gagapps/medadh/fragments/BluetoothFragment.kt
+++ app/src/main/java/com/gagapps/medadh/fragments/BluetoothFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import com.gagapps.medadh.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -17,7 +19,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [BluetoothFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class BluetoothFragment : Fragment() {
+class BluetoothFragment : Fragment(), View.OnClickListener {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -28,6 +30,7 @@ class BluetoothFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+        
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -54,5 +57,17 @@ class BluetoothFragment : Fragment() {
                         putString(ARG_PARAM2, param2)
                     }
                 }
+    }//end companion
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val btnEnable: Button = view.findViewById(R.id.bt_enable)
+        btnEnable.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        if (v?.id == R.id.bt_enable){
+            Toast.makeText(activity, "Bluetooth Enabled!", Toast.LENGTH_LONG).show()
+        }
     }
 }
